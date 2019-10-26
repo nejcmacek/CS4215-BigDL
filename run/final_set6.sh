1d9f49840903505f0d519542730d77d4e5b5578d
Martijn Vermeulen <martijn01.vermeulen@gmail.com>
2019-10-26 12:31:42 +0200

    Final sets parallel

---
+++ run/final_set6.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./process-run.sh result/experiment1_64_20_0.02_0.001_4 --master spark://10.164.0.2:7077 --runtime 18000  --batchSize 64 --maxEpoch 20 --learningRate 0.02 --lambda 0.001 --numberOfNodes 4
