4fcb39db3b22040f3ea563d806acc46a4bd7065f
Martijn Vermeulen <martijn01.vermeulen@gmail.com>
2019-10-26 12:41:03 +0200

    Modified finalsets

--- run/final_set1.sh
+++ run/final_set1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./process-run.sh result/experiment1_256_5_0.005_0.001_1 --master spark://10.164.0.2:7077 --runtime 28800  --batchSize 256 --maxEpoch 5 --learningRate 0.005 --lambda 0.001 --numberOfNodes 1
+./process-run.sh result/experiment1_256_5_0.005_0.001_2 --master spark://10.164.0.2:7077 --runtime 18000  --batchSize 256 --maxEpoch 5 --learningRate 0.005 --lambda 0.001 --numberOfNodes 2
--- run/final_set5.sh
+++ run/final_set5.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./process-run.sh result/experiment1_128_5_0.02_0.001_4 --master spark://10.164.0.2:7077 --runtime 28800  --batchSize 128 --maxEpoch 5 --learningRate 0.02 --lambda 0.001 --numberOfNodes 4
-./process-run.sh result/experiment1_64_20_0.02_0.001_4 --master spark://10.164.0.2:7077 --runtime 28800  --batchSize 64 --maxEpoch 20 --learningRate 0.02 --lambda 0.001 --numberOfNodes 4
+./process-run.sh result/experiment1_128_5_0.02_0.001_4 --master spark://10.164.0.2:7077 --runtime 18000  --batchSize 128 --maxEpoch 5 --learningRate 0.02 --lambda 0.001 --numberOfNodes 4
